b9500d008b5f10754b2855d7d63a2bdc6734a171
Argun Autalipov <argun63901@gmail.com>
2019-07-14 21:05:52 +0600

    Регистрация переписана в читабельный дорабатываемый код

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,43 +19,37 @@ router.post('/register', (req, res) => {
         return res.status(400).json(errors);
     }
 
-    User.findOne({ email: req.body.email} )
-        .then(user => {
-            if(user){
-                return res.status(400).json({ msg: 'Email already taken by someone' })
-            } else {
-                let newUser = new User({
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password
-                });
+    try {
+        //awaiting to find User in MongoDB
+        user = await User.findOne({ email })
 
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                      if (err) throw err;
-                      newUser.password = hash;
-                      newUser
-                        .save()
-                        .then(user => {
-                            const payload = {
-                                id: user.id,
-                                username: user.username
-                            }
-            
-                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 60*60*24*7*12 }, (err, token) => {
-                                res.json({
-                                    success: true,
-                                    token: "Bearer " + token
-                                });
-                            });
-                        })
-                        .catch(err => console.log(err));
-                    });
-                });
-            }
+        //if exist return status 400 to client
+        if(user){
+            return res.status(400).json({ emailRegistered: "This email is already taken by someone."});
+        }
 
+        //else:
+        //generate salt and hash according to the password taken from client
+        salt = await bcrypt.genSalt(10)
+        hash = await bcrypt.hash(req.body.password, salt)
+        
+        //create object with paramterers taken from client and hashed value as its password
+        let newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash
+        });
 
-        })
+        //save to Mongo
+        user = await newUser.save();
+        //return success message 
+        return req.json({ msg: "Registered succesfully" })
+
+    } catch( err ){
+        console.log(err);
+        return res.status(500);
+    }
+    
 });
 
 router.post('/login', (req, res) => {
